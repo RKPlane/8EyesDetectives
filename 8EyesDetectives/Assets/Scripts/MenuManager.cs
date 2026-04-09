b4137d4f3d4be8276281a17ff20dbbfcb997ecea
TeoNB <tensbi@alumnatflorida.es>
2026-04-10 00:18:35 +0200

    MENU UPDATE
    - He creado una escena específica para el menu
    - He dejado de usar el prefab que realice anteriormente
    - Los botones game y continue llevan al sample escene (Prueba)
    - El settings esta incompleto
    - El exit solo funcionará cuando se realice el build final del juego
    - He creado el script Menumanger para administrar dicha escena
    - El metodo pausegame esta incompleto, es para más adelante, no tiene botón

---
+++ 8EyesDetectives/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject menuOptions;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OpenMenuOptions()
+    {
+        mainMenu.SetActive(false);
+        menuOptions.SetActive(true);
+    }
+
+    public void OpenMainMenu()
+    {
+        mainMenu.SetActive(true);
+        menuOptions.SetActive(false);
+    }
+
+    //Para mas adelante
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        OpenMainMenu();
+	}
+
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ContinueGame()
+    {
+		SceneManager.LoadScene("SampleScene");
+	}
+}
