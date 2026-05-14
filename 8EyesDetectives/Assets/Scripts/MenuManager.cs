8051545a3bbe2330dc915a360f91268586da31e2
TeoNB <tensbi@alumnatflorida.es>
2026-05-14 12:31:29 +0200

    Pause Complete

--- 8EyesDetectives/Assets/Scripts/MenuManager.cs
+++ 8EyesDetectives/Assets/Scripts/MenuManager.cs
@@ -6,6 +6,7 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject menuOptions;
+    public GameObject pauseMenu;
 	public InputActionAsset inputActions;
 	private InputAction pauseAction;
     private bool isPaused = false;
@@ -14,54 +15,60 @@ public class MenuManager : MonoBehaviour
 
 	private void Awake()
 	{
-		var map = inputActions.FindActionMap("UI");
+		var map = inputActions.FindActionMap("UI"); // Asegúrate de que el nombre del Action Map coincida con el que tienes en tu Input Actions
 
-		pauseAction = map.FindAction("Pause");
+		pauseAction = map.FindAction("Pause"); // Asegúrate de que el nombre de la acción coincida con el que tienes en tu Input Actions
 
+        
 	}
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
     {
-
-    }
+       
+	}
 
     // Update is called once per frame
     void Update()
-    {
-        if (pauseAction.IsPressed() && !isPaused){
-            pauseGame();
-        } else if (pauseAction.IsPressed() && isPaused) {
+	{ // Verificar si se ha presionado la tecla de pausa
+		if (pauseAction.WasPerformedThisFrame() && !isPaused)
+		{ 
+			pauseGame();
+            
+		} else if (pauseAction.WasPerformedThisFrame() && isPaused) {
             resumeGame();
+            
         }
     }
 
-    public void OpenMenuOptions()
-    {
+    public void OpenMenuOptions() // Método para abrir el menú de opciones
+	{
         mainMenu.SetActive(false);
-        menuOptions.SetActive(true);
+		pauseMenu.SetActive(false);
+		menuOptions.SetActive(true);
     }
 
-    public void OpenMainMenu()
-    {
+    public void OpenMainMenu() // Método para volver al menú principal
+	{
         mainMenu.SetActive(true);
         menuOptions.SetActive(false);
-    }
+		pauseMenu.SetActive(false);
+	}
 
     //Para mas adelante
-    public void pauseGame()
-    {
+    public void pauseGame() // Método para pausar el juego
+	{
         Time.timeScale = 0;
-        OpenMainMenu();
-        isPaused = true;
+		pauseMenu.SetActive(true);
+		isPaused = true;
         Debug.Log("Paused");
 	}
 
-    public void resumeGame()
-    {
+    public void resumeGame() // Método para reanudar el juego
+	{
         Time.timeScale = 1;
-        OpenMainMenu();
         isPaused = false;
         Debug.Log("Resumed");
+		pauseMenu.SetActive(false);
 	}
 	public void QuitGame()
     {
@@ -73,8 +80,5 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene("Nivel0");
     }
 
-    public void ContinueGame()
-    {
-		SceneManager.LoadScene("WebTest");
-	}
+    
 }
