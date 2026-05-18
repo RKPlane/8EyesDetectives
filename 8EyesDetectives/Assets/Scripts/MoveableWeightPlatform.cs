7d35fb8d95fa419d5879be751621803b24bc2795
RKPlane <115335583+RKPlane@users.noreply.github.com>
2026-05-18 12:51:07 +0200

    Rework a las plataformas y arreglo de levels final

--- 8EyesDetectives/Assets/Scripts/MoveableWeightPlatform.cs
+++ 8EyesDetectives/Assets/Scripts/MoveableWeightPlatform.cs
@@ -6,22 +6,26 @@ public class MoveableWeightPlatform : MonoBehaviour
     public bool unweighted = false;
     public float weight;
 
-	[SerializeField] private GameObject target;
+    [SerializeField] private GameObject target;
 
-	Vector3 origin;
-	Vector3 distance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Vector3 origin;
+    Vector3 distance;
+    Vector3 previousPosition;
+
+    private Rigidbody2D playerRb = null;
+
     private void Awake()
     {
-		origin = transform.position;
-	}
+        origin = transform.position;
+    }
+
     void Start()
     {
         distance = transform.position - target.transform.position;
+        previousPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (weighted)
         {
@@ -31,25 +35,34 @@ public class MoveableWeightPlatform : MonoBehaviour
         {
             MoveBack();
         }
+
+        // Move
+        if (playerRb != null)
+        {
+            Vector2 delta = (Vector2)transform.position - (Vector2)previousPosition;
+            if (delta != Vector2.zero) { playerRb.position += delta; }
+        }
+
+        previousPosition = transform.position;
     }
 
     public void Move()
     {
-		Vector3 targetPosition = target.transform.position - distance;
-		Vector3 lerpTarget = Vector3.Lerp(origin, targetPosition, weight);
-		transform.position = Vector3.Lerp(transform.position, lerpTarget, 2f * Time.deltaTime);
+        Vector3 targetPosition = target.transform.position - distance;
+        Vector3 lerpTarget = Vector3.Lerp(origin, targetPosition, weight);
+        transform.position = Vector3.Lerp(transform.position, lerpTarget, 2f * Time.fixedDeltaTime);
     }
 
     public void MoveBack()
     {
-        transform.position = Vector2.Lerp(transform.position, origin, Time.deltaTime);
+        transform.position = Vector2.Lerp(transform.position, origin, Time.fixedDeltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.transform.SetParent(this.transform);
+            playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
         }
     }
 
@@ -57,10 +70,7 @@ public class MoveableWeightPlatform : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (gameObject.activeInHierarchy && collision.gameObject.activeInHierarchy)
-            {
-                collision.transform.SetParent(null);
-            }
+            playerRb = null;
         }
     }
 }
