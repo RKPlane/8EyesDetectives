90b317dc0e89b5bdbb588660a758dbd34fb0bf78
RKPlane <115335583+RKPlane@users.noreply.github.com>
2026-05-21 19:28:05 +0200

    GAMEFEST RUN VERSION

--- 8EyesDetectives/Assets/Scripts/MenuManager.cs
+++ 8EyesDetectives/Assets/Scripts/MenuManager.cs
@@ -6,74 +6,82 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject menuOptions;
-	public GameObject pauseMenu;
-	public InputActionAsset inputActions;
-	private InputAction pauseAction;
+    public GameObject pauseMenu;
+
+    public InputActionAsset inputActions;
+    private InputAction pauseAction;
+
     private bool isPaused = false;
 
-
-
-	private void Awake()
-	{
-		var map = inputActions.FindActionMap("UI");
-
-		pauseAction = map.FindAction("Pause");
-
-	}
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
-	void Start()
+    void Awake()
     {
+        var map = inputActions.FindActionMap("UI");
 
+        pauseAction = map.FindAction("Pause");
+
+        map.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        if (pauseAction.WasPerformedThisFrame() && !isPaused){
-            pauseGame();
-        } else if (pauseAction.WasPerformedThisFrame() && isPaused) {
-            resumeGame();
-        }
+        pauseAction.Enable();
+        pauseAction.performed += OnPausePressed;
+    }
+
+    void OnDisable()
+    {
+        pauseAction.performed -= OnPausePressed;
+        pauseAction.Disable();
+    }
+
+    private void OnPausePressed(InputAction.CallbackContext ctx)
+    {
+        if (!isPaused)
+            PauseGame();
+        else
+            ResumeGame();
+    }
+
+    public void PauseGame()
+    {
+        if (isPaused) return;
+
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        if (!isPaused) return;
+
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
     }
 
     public void OpenMenuOptions()
     {
         mainMenu.SetActive(false);
         menuOptions.SetActive(true);
-		pauseMenu.SetActive(false);
-	}
+        pauseMenu.SetActive(false);
+    }
 
     public void OpenMainMenu()
     {
         mainMenu.SetActive(true);
         menuOptions.SetActive(false);
-		pauseMenu.SetActive(false);
-	}
+        pauseMenu.SetActive(false);
+    }
 
-    //Para mas adelante
-    public void pauseGame()
-    {
-        Time.timeScale = 0;
-		pauseMenu.SetActive(true);
-		isPaused = true;
-        Debug.Log("Paused");
-	}
-
-    public void resumeGame()
-    {
-        Time.timeScale = 1;
-		pauseMenu.SetActive(false);
-		isPaused = false;
-        Debug.Log("Resumed");
-	}
-	public void QuitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
 
     public void StartGame()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Tutorial");
     }
-
 }
